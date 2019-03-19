dabf2bc66a75a49b63d7c957bcd7a053cf46d8a7
fabioNog <fabio.carmo@computacao.ufla.br>
2019-03-19 15:45:37 -0300

    modify name and schemas

--- backend/src/api/billingCycle/billingCycle.js
+++ backend/src/api/billingCycle/billingCycle.js
@@ -30,46 +30,47 @@ const mongoose = restful.mongoose
 //     locations: { type: String, required: true },
 // })
 
-// const VauncherSchema = new mongoose.Schema({    
-//     start_day: { type: Number, min: 1, max: 31, required: true },
-//     start_month: { type: Number, min: 1, max: 12, required: true },
-//     start_year: { type: Number, min: 1970, max: 2100, required: true },
-//     end_day: { type: Number, min: 1, max: 31, required: true },
-//     end_month: { type: Number, min: 1, max: 12, required: true },
-//     end_year: { type: Number, min: 1970, max: 2100, required: true },    
-//     unlimited : { type: Boolean, required: true },
-//     justNew : { type: Boolean, required: true },
-//     service : [services], //Serviços que se pode usar ‘Unhas’, ‘Depilação’
-//     code : { type: String, required: true }, //Código que deve ser utilizado
-//     usedBy : [usedbys],
-//     campaign : { type: Boolean, required: true }, //se for Dia dos pais então true
-//     location : [locations], //Em quais cidades pode ser utilizado, ‘São Paulo’, ‘Rio de Janeiro’
-//     usedTimes : { type: Number, min: 0, required: [true, 'Informe o quantidade de vezes usada!'] }, //Quantidade de vezes utilizado
-//     timesByUser : { type: Number, min: 1, required: [true, 'Informe o quantidade de vezes que podera ser usatilizado por cliente!'] }, //Quantidade de vezes que pode ser utilizado por um cliente
-//     type : "P", //P para porcentagem e V para valor fixo
-//     value : { type: Number, min: 0, required: [true, 'Informe o valor do débito!'] },
-//     maxTimes: { type: Number, min: 0, required: [true, 'Informe o valor do débito!'] }, //Máximo de utilizações
-// });
+const VauncherSchema = new mongoose.Schema({ 
+    name: { type: String, required: true },   
+    // start_day: { type: Number, min: 1, max: 31, required: true },
+    // start_month: { type: Number, min: 1, max: 12, required: true },
+    // start_year: { type: Number, min: 1970, max: 2100, required: true },
+    // end_day: { type: Number, min: 1, max: 31, required: true },
+    // end_month: { type: Number, min: 1, max: 12, required: true },
+    // end_year: { type: Number, min: 1970, max: 2100, required: true },    
+    // // unlimited : { type: Boolean, required: true },
+    // // justNew : { type: Boolean, required: true },
+    // services : [services], //Serviços que se pode usar ‘Unhas’, ‘Depilação’
+    // code : { type: String, required: true }, //Código que deve ser utilizado
+    // usedBy : [usedbys],
+    // // campaign : { type: Boolean, required: true }, //se for Dia dos pais então true
+    // location : [locations], //Em quais cidades pode ser utilizado, ‘São Paulo’, ‘Rio de Janeiro’
+    // // usedTimes : { type: Number, min: 0, required: [true, 'Informe o quantidade de vezes usada!'] }, //Quantidade de vezes utilizado
+    // // timesByUser : { type: Number, min: 1, required: [true, 'Informe o quantidade de vezes que podera ser usatilizado por cliente!'] }, //Quantidade de vezes que pode ser utilizado por um cliente
+    // // type : "P", //P para porcentagem e V para valor fixo
+    // // value : { type: Number, min: 0, required: [true, 'Informe o valor do débito!'] },
+    // // maxTimes: { type: Number, min: 0, required: [true, 'Informe o valor do débito!'] }, //Máximo de utilizações
+});
 
 
-const creditSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    value: { type: Number, min: 0, required: true }
-})
+// const creditSchema = new mongoose.Schema({
+//     name: { type: String, required: true },
+//     value: { type: Number, min: 0, required: true }
+// })
 
-const debtSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    value: { type: Number, min: 0, required: [true, 'Informe o valor do débito!'] },
-    status: { type: String, required: false, uppercase: true,
-        enum: ['PAGO', 'PENDENTE', 'AGENDADO'] }
-})
+// const debtSchema = new mongoose.Schema({
+//     name: { type: String, required: true },
+//     value: { type: Number, min: 0, required: [true, 'Informe o valor do débito!'] },
+//     status: { type: String, required: false, uppercase: true,
+//         enum: ['PAGO', 'PENDENTE', 'AGENDADO'] }
+// })
 
-const billingCycleSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    month: { type: Number, min: 1, max: 12, required: true },
-    year: { type: Number, min: 1970, max: 2100, required: true },
-    credits: [creditSchema],
-    debts: [debtSchema]
-})
+// const billingCycleSchema = new mongoose.Schema({
+//     name: { type: String, required: true },
+//     month: { type: Number, min: 1, max: 12, required: true },
+//     year: { type: Number, min: 1970, max: 2100, required: true },
+//     credits: [creditSchema],
+//     debts: [debtSchema]
+// })
 
-module.exports = restful.model('Vouchers', billingCycleSchema)
+module.exports = restful.model('Vouchers2', VauncherSchema)
